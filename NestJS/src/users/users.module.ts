453198c9eb097574bcecebf208393eef072714fc
sicp618 <cc191954@gmail.com>
2024-01-05 19:34:28 +0800

    nestjs

---
+++ NestJS/src/users/users.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+
+@Module({
+  providers: [UsersService],
+  controllers: [UsersController]
+})
+export class UsersModule {}
+
+export class User {
+  id: number;
+  username: string;
+  password: string;
+  email: string;
+}
