e9f501bb48b4017788785adc298be4f6d9f928a0
sicp618 <cc191954@gmail.com>
2024-01-25 15:19:00 +0800

    add axios to make http requests

--- nextjs/app/counter.tsx
+++ nextjs/app/counter.tsx
@@ -1,44 +1,70 @@
 "use client";
 
+import axios from "axios";
 import { useState } from "react";
 
-const registerAction = async () => {
-  const data = {
-    user: {
-      username: "user2",
-      password: "123456",
-      email: "user2@mail",
-      id: 0,
-    },
-  };
-  const body = JSON.stringify(data);
-  try {
-    const res = await fetch("http://localhost:3000/api/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: body,
-    });
+const registerUrl = `${process.env.NEXT_PUBLIC_API_HOST}/api/register`;
+const loginUrl = `${process.env.NEXT_PUBLIC_API_HOST}/api/login`;
 
-    if (!res.ok) {
-      throw new Error(res.statusText);
-    }
-
-    const result = await res.json();
-    console.log("res", result);
-  } catch (error) {
-    console.log("error", error);
-  }
+type User = {
+  username: string;
+  password: string;
+  email: string;
+  id: number;
 };
 
 export default function Counter() {
-  const [count, setCount] = useState(0);
+  const [user, setUser] = useState<User | null>(null);
+
+  const registerAction = async () => {
+    const data = {
+      user: {
+        username: "user2",
+        password: "123456",
+        email: "user2@mail",
+        id: 0,
+      },
+    };
+    axios
+      .post(registerUrl, data)
+      .then((res) => {
+        setUser(res.data.user);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
+
+  const loginAction = async () => {
+    console.log("loginAction", loginUrl, process.env.NEXT_PUBLIC_API_HOST, process.env);
+    const loginData = {
+      user: {
+        username: "user2",
+        password: "123456",
+        email: "user2@mail",
+        id: 0,
+      },
+    };
+    axios
+      .post(loginUrl, loginData)
+      .then((res) => {
+        console.log("res", res);
+        setUser(res.data.user);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
 
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <button onClick={registerAction}>Click me</button>
+      <button onClick={registerAction}>register</button>
+      <p />
+      <button onClick={loginAction} hidden={!!user}>
+        login
+      </button>
+      <p />
+      <label>{user ? user.username : ""}</label>
     </div>
   );
 }
