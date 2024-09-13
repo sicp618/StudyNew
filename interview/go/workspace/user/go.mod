f36db19aebfc252929fac1133ccdfc49f44ec17a
sicp618 <cc191954@gmail.com>
2024-09-13 10:07:40 +0900

    add redis and postgresql

--- interview/go/workspace/user/go.mod
+++ interview/go/workspace/user/go.mod
@@ -2,14 +2,28 @@ module github.com/sicp618/interview/user
 
 go 1.21.0
 
+require github.com/sicp618/interview/proto v0.0.0
+
 require (
-	github.com/sicp618/interview/proto v0.0.0
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.1 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
 	golang.org/x/net v0.26.0 // indirect
-	golang.org/x/sys v0.21.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
 	google.golang.org/grpc v1.66.2 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
+	gorm.io/driver/postgres v1.5.9 // indirect
+	gorm.io/gorm v1.25.12 // indirect
 )
 
 replace github.com/sicp618/interview/proto => ../proto/gen
--- interview/go/workspace/user/main.go
+++ interview/go/workspace/user/main.go
@@ -5,35 +5,97 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 
+	"github.com/go-redis/redis/v8"
 	pb "github.com/sicp618/interview/proto/user"
-    "google.golang.org/grpc/codes"
-    "google.golang.org/grpc/status"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+var db  *gorm.DB
+var redisClient *redis.Client
+
+func initPostgres() {
+    var err error
+    dsn := "postgres://user:password@localhost:5432/nostr?TimeZone=Asia/Shanghai"
+    db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatalf("failed to connect to PostgreSQL: %v", err)
+    }
+
+    // 自动迁移模式
+    err = db.AutoMigrate(&User{})
+    if err != nil {
+        log.Fatalf("failed to migrate database: %v", err)
+    }
+}
+
+func initRedis() {
+    redisClient = redis.NewClient(&redis.Options{
+        Addr: "localhost:6379",
+    })
+}
+
+type User struct {
+    ID    int32  `gorm:"primaryKey"`
+    Name  string `gorm:"column:name"`
+    Email string `gorm:"column:email"`
+}
 
 type Server struct {
 	pb.UnimplementedUserServiceServer
 }
 
-func (s *Server) GetUser(ctx context.Context, in *pb.UserRequest) (*pb.UserResponse, error) {
-	if in.Id == 1 {
-		return &pb.UserResponse {
-			Data: &pb.UserInfo {
-				Id: 1,
-				Name: "jack",
+func (s *Server) GetUser(ctx context.Context, req *pb.UserRequest) (*pb.UserResponse, error) {
+		cacheKey := fmt.Sprintf("user:%d", req.Id)
+		userCache, err := redisClient.Get(ctx, cacheKey).Result()
+		if err == redis.Nil {
+			var user User
+			result := db.First(&user, req.Id)
+			if result.Error != nil {
+				if result.Error == gorm.ErrRecordNotFound {
+					return nil, status.Errorf(codes.NotFound, "user not found")
+				}
+				return nil, status.Errorf(codes.Internal, "failed to query user: %v", result.Error)
+			}
+	
+			err = redisClient.Set(ctx, cacheKey, fmt.Sprintf("%s,%s", user.Name, user.Email), time.Hour).Err()
+			if err != nil {
+				log.Printf("failed to set cache: %v", err)
+			}
+	
+			return &pb.UserResponse{
+				Data: &pb.UserInfo{
+					Id:    user.ID,
+					Name:  user.Name,
+					Email: user.Email,
+				},
+			}, nil
+		} else if err != nil {
+			return nil, status.Errorf(codes.Internal, "failed to get cache: %v", err)
+		}
+	
+		var name, email string
+		fmt.Sscanf(userCache, "%s,%s", &name, &email)
+	
+		return &pb.UserResponse{
+			Data: &pb.UserInfo{
+				Id:    req.Id,
+				Name:  name,
+				Email: email,
 			},
 		}, nil
-	}
-
-	return nil, status.Errorf(codes.NotFound, "no find")
 }
 
-
-
 func main() {
-    fmt.Println("Starting gRPC server...")
+	initPostgres()
+    initRedis()
 
+    fmt.Println("Starting gRPC server...")
     lis, err := net.Listen("tcp", ":50051")
     if err != nil {
         log.Fatalf("failed to listen: %v", err)
